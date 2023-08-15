f1a82a94ad01a50ea05f8bddc5715807e26f6437
BrunoVian <bruno.vian@edu.unipar.br>
2023-08-14 21:51:33 -0300

    Exercício 02

---
+++ DesignPatterns_Adapter_Exercicio/src/main/java/com/mycompany/designpatterns_adapter_exercicio/gmailClient/GmailClient.java
@@ -0,0 +1,12 @@
+package com.mycompany.designpatterns_adapter_exercicio.gmailClient;
+
+public class GmailClient {
+
+    public void login() {
+        System.out.println("Entrando no Gmail");
+    }
+
+    public void getEmails() {
+        System.out.println("Buscando e-mails");
+    }
+}
